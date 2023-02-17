53350fe13a8dc835cb89e37af192e29c879a0aea
David Isfeld <56812996+DavidIsfeld@users.noreply.github.com>
2023-02-17 14:11:09 -0500

    Begin handling events on input and button press

--- src/App.js
+++ src/App.js
@@ -4,22 +4,40 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      <ParticlesBg type="color" bg={true} />
-      {
-        /*
-        <FaceRecognition />
-        */
-      }
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event);
+  }
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <ParticlesBg type="color" bg={true} />
+        {
+          /*
+          <FaceRecognition />
+          */
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
