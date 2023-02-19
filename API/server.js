0bd6935ae7b38f75428e23ac8db7aa7e4ae2545f
David Isfeld <56812996+DavidIsfeld@users.noreply.github.com>
2023-02-19 14:53:09 -0500

    Skeletal password hashing

--- API/server.js
+++ API/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require('bcryptjs');
 
 const app = express();
 
@@ -30,25 +31,31 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json('success');
-    } else {
-        res.status(400).json("error logging in");
-    }
+    bcrypt.compare(req.body.password, database.users[database.users.length-1].password, function(err, resB) {
+        if (resB) {
+            res.json('success');
+        } else {
+            res.status(400).json("error logging in");
+        }
+    });
 });
 
 app.post('/register', (req, res) => {
     const {email, name, password} = req.body;
-    database.users.push({
-        id: "125",
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            database.users.push({
+                id: "125",
+                name: name,
+                email: email,
+                password: hash,
+                entries: 0,
+                joined: new Date()
+            });
+        
+            res.json(database.users[database.users.length - 1]);
+        });
     });
-
-    res.json(database.users[database.users.length - 1]);
 });
 
 app.get('/profile/:id', (req, res) => {
